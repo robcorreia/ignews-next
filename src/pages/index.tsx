6a0310b8cb593ea52ff662bd7356f9151899bf9e
Robson <robccorreia@gmail.com>
2022-05-03 23:01:15 -0300

    feat: alter for getStaticProps

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,12 @@
-import { GetServerSideProps } from "next";
+import {  GetStaticProps } from "next";
 import Head from "next/head";
 import { SubscribeButton } from "../components/SubscribeButton";
 import { stripe } from "../services/stripe";
 import styles from "./home.module.scss";
+
+// client-side CSR
+// server-side SSR
+// static site generation SSG
 
 interface HomeProps {
   product: {
@@ -36,7 +40,7 @@ export default function Home({ product }: HomeProps) {
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve("price_1KvVphATnDcf6i9DK5MWFnip");
 
   const product = {
@@ -51,5 +55,6 @@ export const getServerSideProps: GetServerSideProps = async () => {
     props: {
       product,
     },
+    revalidate: 60 * 60 * 24, // 24 horas
   };
 };
